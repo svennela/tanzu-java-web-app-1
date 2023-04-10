6b2c2addd93c3fad7d0b3da6cc16e04ce27189cb
nycpivot <mijames@vmware.com>
2023-04-10 16:28:53 +0000

    Fixed failing test.

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greeting from Spring Boot + Tanzu!";
+		return "Greetings from Spring Boot + Tanzu!";
 	}
 
 }
